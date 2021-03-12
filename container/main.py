c757ddb9af8db969881d4e03d3792441161f3972
Matheus C. França <matheus-catarino@hotmail.com>
2021-03-12 09:37:04 -0300

    Zig compiler

--- container/main.py
+++ container/main.py
@@ -9,9 +9,9 @@ from werkzeug.wrappers import Request, Response
 @dispatcher.add_method
 def execute(source: str) -> dict:
     os.chdir(tempfile.mkdtemp())
-    with open("main.cpp", "w") as f:
+    with open("main.zig", "w") as f:
         f.write(source)
-    subprocess.run(["g++", "-march=native", "-O2", "main.cpp"])
+    subprocess.run(["zig", "build-exe", "-femit-bin=./a.out", "-OReleaseSafe", "main.zig"])
     result = subprocess.run("./a.out", capture_output=True, timeout=10)
     limit = 1000
     return result.stdout[:limit].decode('utf-8').strip()
